3c11a59fd11e92dd20b439ca46d7a4da9e18f6b5
Igor Túllio <igor.goncalves@basis.com.br>
2018-07-13 13:59:58 -0300

    Salvando novo cliente

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {CidadeService} from "../../services/domain/cidade.service";
 import {EstadoService} from "../../services/domain/estado.service";
 import {EstadoDTO} from "../../models/estado.dto";
 import {CidadeDTO} from "../../models/cidade.dto";
+import {ClienteService} from "../../services/domain/cliente.service";
+import {AlertController} from 'ionic-angular/components/alert/alert-controller';
 
 @IonicPage()
 @Component({
@@ -17,11 +19,17 @@ export class SignupPage {
   estados: EstadoDTO[];
   cidades: CidadeDTO[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder, public cidadeService: CidadeService, public estadoService: EstadoService) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public formBuilder: FormBuilder,
+              public cidadeService: CidadeService,
+              public estadoService: EstadoService,
+              public clienteService: ClienteService,
+              public alertCtrl: AlertController) {
     this.formGroup = this.formBuilder.group({
       nome: ['Joaquim', [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
       email: ['joaquim@gmail.com', [Validators.required, Validators.email]],
-      tipo : ['1', [Validators.required]],
+      tipoCliente : ['1', [Validators.required]],
       cpfOuCnpj : ['06134596280', [Validators.required, Validators.minLength(11), Validators.maxLength(14)]],
       senha : ['123', [Validators.required]],
       logradouro : ['Rua Via', [Validators.required]],
@@ -60,7 +68,29 @@ export class SignupPage {
   }
 
   signupUser(){
-    console.log("Enviou o form");
+    this.clienteService.insert(this.formGroup.value).subscribe(
+      response => {
+        this.showInsertOk();
+      },
+      error => {}
+    );
+  }
+
+  showInsertOk() {
+    let alert = this.alertCtrl.create({
+      title: 'Sucesso!',
+      message: 'Cadastro efetuado com sucesso',
+      enableBackdropDismiss: false,
+      buttons: [
+        {
+          text: 'Ok',
+          handler: () => {
+            this.navCtrl.pop();
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 
 }
