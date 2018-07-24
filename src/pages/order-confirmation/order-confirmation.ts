9bcbe89376b2882fd8a6d12cf039a59f805ff64f
Igor Túllio <igor.goncalves@basis.com.br>
2018-07-24 16:30:59 -0300

    Informando pedido registrado ao usuário

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -19,6 +19,7 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
+  codPedido: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public clienteService: ClienteService, public cartService: CartService, public pedidoService: PedidoService) {
     this.pedido = this.navParams.get('pedido');
@@ -49,11 +50,15 @@ export class OrderConfirmationPage {
     this.navCtrl.setRoot('CartPage');
   }
 
+  home(){
+    this.navCtrl.setRoot('CategoriasPage');
+  }
+
   checkout(){
     this.pedidoService.insert(this.pedido).subscribe(
       response => {
         this.cartService.createOrClearCart();
-        console.log(response.headers.get('location'));
+        this.codPedido = this.extractId(response.headers.get('location'));
       },
       error => {
         if (error.status == 403){
@@ -61,6 +66,11 @@ export class OrderConfirmationPage {
         }
       }
     )
+  }
+
+  private extractId(location: string) : string {
+    let position = location.lastIndexOf('/');
+    return location.substring(position + 1, location.length);
   }
 
 }
