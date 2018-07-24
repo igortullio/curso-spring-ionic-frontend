65d7a1f6dfcd6d935d6c89b9a9df7fd28438e079
Igor Túllio <igor.goncalves@basis.com.br>
2018-07-24 16:17:12 -0300

    Salvando pedido

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -6,6 +6,7 @@ import {EnderecoDTO} from "../../models/endereco.dto";
 import {ClienteDTO} from "../../models/cliente.dto";
 import {ClienteService} from "../../services/domain/cliente.service";
 import {CartService} from "../../services/domain/cart.service";
+import {PedidoService} from "../../services/domain/pedido.service";
 
 @IonicPage()
 @Component({
@@ -19,7 +20,7 @@ export class OrderConfirmationPage {
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public clienteService: ClienteService, public cartService: CartService) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public clienteService: ClienteService, public cartService: CartService, public pedidoService: PedidoService) {
     this.pedido = this.navParams.get('pedido');
   }
 
@@ -43,4 +44,23 @@ export class OrderConfirmationPage {
   total() : number {
     return this.cartService.total();
   }
+
+  back(){
+    this.navCtrl.setRoot('CartPage');
+  }
+
+  checkout(){
+    this.pedidoService.insert(this.pedido).subscribe(
+      response => {
+        this.cartService.createOrClearCart();
+        console.log(response.headers.get('location'));
+      },
+      error => {
+        if (error.status == 403){
+          this.navCtrl.setRoot('HomePage');
+        }
+      }
+    )
+  }
+
 }
