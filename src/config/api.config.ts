c0de1ac02bab2456b01f927ef33857c7343facb4
Igor Túllio <igor.goncalves@basis.com.br>
2018-08-17 16:34:47 -0300

    Testando o app no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,5 @@
 export const API_CONFIG = {
-  baseUrl: "http://localhost:8080",
+  // baseUrl: "http://localhost:8080",
+  baseUrl: "https://igortullio-curso-spring-ionic.herokuapp.com",
   bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/igortullio-cursomc"
 }
